9e145395196017927375df2ee9e981d0417d9013
Andrew Timmony <andrew.timmony@gmail.com>
2014-03-18 22:20:05 +1100

    updates

--- Test/src/main/java/com/timmony/notification/test/MainActivity.java
+++ Test/src/main/java/com/timmony/notification/test/MainActivity.java
@@ -5,6 +5,9 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -35,19 +38,21 @@ public class MainActivity extends Activity implements OnClickListener{
 
     @Override
     public void onClick(View v){
+        Bitmap bm = BitmapFactory.decodeResource(getResources(), R.drawable.ic_noti_large);
         Intent intent = new Intent();
         PendingIntent pIntent = PendingIntent.getActivity(this, 0, intent, 0);
         Notification noti;
         noti = new Notification.Builder(this)
                 .setTicker("Your NetCode to authorise the payment of $200.00 to BSB 062000 Account number ****1234 is 123456.")
-                .setContentTitle("CommBank")
+                .setContentTitle("CommBank app")
                 .setContentText("Your NetCode to authorise the payment of $200.00 to BSB 062000 Account number ****1234 is 123456.")
-                .setSmallIcon(R.drawable.ic_launcher)
+                .setSmallIcon(R.drawable.ic_notifi_small)
                 .setStyle(new Notification.BigTextStyle()
                 .bigText("Your NetCode to authorise the payment of $200.00 to BSB 062000 Account number ****1234 is 123456."))
-                .setDefaults(Notification.DEFAULT_ALL) //
-                //.setLargeIcon(R.drawable.ic_launcher)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setLargeIcon(bm)
                 .setContentIntent(pIntent).build();
+        noti.largeIcon=bm;
         noti.flags=Notification.FLAG_AUTO_CANCEL;
         NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
         notificationManager.notify(0, noti);
